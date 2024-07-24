0cd374b77189d5b83d85eaade014615fa100d150
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-07-24 20:50:36 +0200

    Updated single quote combo term

--- users/pket/combos.c
+++ users/pket/combos.c
@@ -23,58 +23,48 @@ bool combo_should_trigger(
 }
 
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
-    uint16_t term;
     switch (index) {
-        case WP_CBR_PAIR:
-        case WF_LCBR:
-        case FP_RCBR:
-            term = 45;
-            break;
 
         case RAI_ENT:
         case RS_LPRN:
         case ST_RPRN:
         case RT_PRN_PAIR:
-
         case XC_COPY:
         case CD_PASTE:
         case LU_QUES_DOT:
-
         case LUY_SNAKE_SCREAM:
-            term = 30;
-            break;
+            return 30;
 
-        case VCB_LN:
         case VCB_NH:
         case XD_CUT:
         case ZX_UNDO:
         case UY_QUOT:
         case EI_TAB:
         case NI_EQL:
-            term = 40;
-            break;
+            return 40;
 
         case NE_ESC:
-            term = 50;
-            break;
+            return 50;
 
+        case VCB_LN:
+            return 55;
+
+        case WP_CBR_PAIR:
+        case WF_LCBR:
+        case FP_RCBR:
         case HCOM_DQUOT:
         case SWE_AO:
         case SWE_AE:
         case SWE_OE:
-            term = 45;
-            break;
-
         case LTGT_ARROW:
         case XCD_PASTE_SFT:
         case WFP_CBR_PAIR_IN:
         case RST_PRN_PAIR_IN:
         case NEI_ENT:
+            return 45;
 
         default:
-            term = 45;
-            break;
+            return 45;
     }
-    return term;
 }
 
